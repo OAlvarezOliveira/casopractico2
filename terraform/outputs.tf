f2c2cbbfd448046cc37596b3334a8fd5cb8c98b7
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-06 17:23:08 +0200

    Añadiendo Outputs.tf para revision de infra creada

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "acr_admin_pass" {
+  value = azurerm_container_registry.acr.admin_password
+}
+
+output "acr_admin_user" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "ssh_user" {
+  value = azurerm_linux_virtual_machine.vm.admin_username
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
