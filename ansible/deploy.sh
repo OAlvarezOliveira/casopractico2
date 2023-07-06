44ed19b051318fd351542987708a11311a2beb76
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-06 21:47:01 +0200

    Añadimos el script de deploy para los playbooks de ansible

---
+++ ansible/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Lista de playbooks a ejecutar
+playbooks=("01_playbook.yaml" "02_playbook2.yaml")
+
+# Ruta al inventario de Ansible
+inventory="hosts.txt"
+
+# Recorre la lista de playbooks y ejecútalos
+for playbook in "${playbooks[@]}"
+do
+    echo "Ejecutando playbook: $playbook"
+    ansible-playbook -i "$inventory" -l podman_vm "$playbook"
+    echo "Playbook $playbook completado"
+done
