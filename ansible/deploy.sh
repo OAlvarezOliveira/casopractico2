1efbe94830b07cc1f0ce04a14b069fb1276e1ada
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-11 01:22:41 +0200

    Update de container-web.service

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #eliminar una clave de host específica del archivo known_hosts
-ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "51.142.226.203"
+ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.26.197.104"
 
 #evitar la verificación del fingerprint al conectarte a ese host
-ssh-keyscan -H 51.142.226.203 >> ~/.ssh/known_hosts
+ssh-keyscan -H 20.26.197.104 >> ~/.ssh/known_hosts
 
 # Lista de playbooks a ejecutar
 ansible-playbook -i hosts.txt 01_playbook.yaml
