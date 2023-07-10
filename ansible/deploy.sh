e0252d315be26ae7078c16b1dcbc2afe592a56db
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-11 01:00:19 +0200

    add template para container-web.service

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
 #eliminar una clave de host específica del archivo known_hosts
-ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "13.87.77.196"
+ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "51.142.226.203"
 
 #evitar la verificación del fingerprint al conectarte a ese host
-ssh-keyscan -H 13.87.77.196 >> ~/.ssh/known_hosts
-
+ssh-keyscan -H 51.142.226.203 >> ~/.ssh/known_hosts
 
 # Lista de playbooks a ejecutar
-playbooks=("01_playbook.yaml" "02_playbook.yaml")
-
-# Ruta al inventario de Ansible
-inventory="hosts.txt"
-
-# Recorre la lista de playbooks y ejecútalos
-for playbook in "${playbooks[@]}"
-do
-    echo "Ejecutando playbook: $playbook"
-    ansible-playbook -i "$inventory" -l podman_vm "$playbook"
-    echo "Playbook $playbook completado"
-done
+ansible-playbook -i hosts.txt 01_playbook.yaml
