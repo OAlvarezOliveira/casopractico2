379dcf6c82848b4bad2711a0b2abe3f06f460caf
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-11 18:58:12 +0200

    Eliminar opcion templates y correccion de errores

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#eliminar una clave de host específica del archivo known_hosts
+# Eliminar una clave de host específica del archivo known_hosts
 ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.26.197.104"
 
-#evitar la verificación del fingerprint al conectarte a ese host
+# Evitar la verificación del fingerprint al conectarte a ese host
 ssh-keyscan -H 20.26.197.104 >> ~/.ssh/known_hosts
 
-# Lista de playbooks a ejecutar
+# Lista de playbooks a ejecutar para la VM con Podman
 ansible-playbook -i hosts.txt 00_playbook.yaml
+ansible-playbook -i hosts.txt 01_playbook.yaml
+ansible-playbook -i hosts.txt 02_playbook.yaml
