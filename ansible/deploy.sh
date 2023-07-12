e5b1dfdec02599ee67fc9a227162de4255ea5073
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-12 14:35:15 +0200

    Init Grafana sobre AKS

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Eliminar una clave de host específica del archivo known_hosts
-ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.26.197.104"
+ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.254.94.254"
 
 # Evitar la verificación del fingerprint al conectarte a ese host
-ssh-keyscan -H 20.26.197.104 >> ~/.ssh/known_hosts
+ssh-keyscan -H 20.254.94.254 >> ~/.ssh/known_hosts
 
 # Lista de playbooks a ejecutar para la VM con Podman
 ansible-playbook -i hosts.txt 00_playbook.yaml --extra-vars "@vars.yaml"
