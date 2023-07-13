d51c3932c1743ac145fa7530bc271d15a20a879c
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-13 17:29:56 +0200

    Update deploy para pruebas finales

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -8,9 +8,9 @@ ssh-keyscan -H 20.68.151.93 >> ~/.ssh/known_hosts
 
 
 # Lista de playbooks a ejecutar para la VM con Podman
-#ansible-playbook -i hosts.txt 00_playbook.yml --extra-vars "@vars.yaml"
-#ansible-playbook -i hosts.txt 01_playbook.yml --extra-vars "@vars.yaml"
-#ansible-playbook -i hosts.txt 02_playbook.yml --extra-vars "@vars.yaml"
+ansible-playbook -i hosts.txt 00_playbook.yml --extra-vars "@vars.yaml"
+ansible-playbook -i hosts.txt 01_playbook.yml --extra-vars "@vars.yaml"
+ansible-playbook -i hosts.txt 02_playbook.yml --extra-vars "@vars.yaml"
 
 # Lista de playbooks a ejecutar para la AzureVote con Redis
  ansible-playbook -i hosts.txt 03_playbook.yml --extra-vars "@vars.yaml"  
