6d7f8c5fe46bbdf0617facc866bbcee25ab90587
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-13 16:50:13 +0200

    Update de Imagenes Custom para AzureVote y Redis

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,15 +2,16 @@
 
 # Eliminar una clave de host específica del archivo known_hosts
 ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.68.151.93"
-ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.0.224.27"
-
 
 # Evitar la verificación del fingerprint al conectarte a ese host
 ssh-keyscan -H 20.68.151.93 >> ~/.ssh/known_hosts
-ssh-keyscan -H 20.0.224.27 >> ~/.ssh/known_hosts
 
 
 # Lista de playbooks a ejecutar para la VM con Podman
-ansible-playbook -i hosts.txt 00_playbook.yml --extra-vars "@vars.yaml"
-ansible-playbook -i hosts.txt 01_playbook.yml --extra-vars "@vars.yaml"
-ansible-playbook -i hosts.txt 02_playbook.yml --extra-vars "@vars.yaml"
+#ansible-playbook -i hosts.txt 00_playbook.yml --extra-vars "@vars.yaml"
+#ansible-playbook -i hosts.txt 01_playbook.yml --extra-vars "@vars.yaml"
+#ansible-playbook -i hosts.txt 02_playbook.yml --extra-vars "@vars.yaml"
+
+# Lista de playbooks a ejecutar para la AzureVote con Redis
+ ansible-playbook -i hosts.txt 03_playbook.yml --extra-vars "@vars.yaml"  
+ ansible-playbook -i hosts.txt 04_playbook.yml --extra-vars "@vars.yaml" 
