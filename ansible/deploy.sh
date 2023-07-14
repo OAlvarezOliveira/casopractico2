bfb5962dba5130c61413dcfdb47ac84da4147335
Óscar Álvarez <oalvarezoliveira@gmail.com>
2023-07-14 13:11:06 +0200

    probando con secretos

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Eliminar una clave de host específica del archivo known_hosts
-ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "20.49.206.104"
+ssh-keygen -f "/home/server_admin/.ssh/known_hosts" -R "51.11.32.202"
 
 # Evitar la verificación del fingerprint al conectarte a ese host
-ssh-keyscan -H 20.49.206.104 >> ~/.ssh/known_hosts
+ssh-keyscan -H 51.11.32.202 >> ~/.ssh/known_hosts
 
 # Asegurate de que ansible/kubeconfig.yaml es el correcto
 az aks get-credentials --resource-group cp2_resource_group --name aks_cluster_kubernetes --file /mnt/d/Minhasse/Documents/Proyectos/casopractico2/ansible/kubeconfig.yaml
@@ -18,5 +18,5 @@ ansible-playbook -i hosts.txt 01_playbook.yml --extra-vars "@vars.yml"
 ansible-playbook -i hosts.txt 02_playbook.yml --extra-vars "@vars.yml"
 
 # Lista de playbooks a ejecutar para la AzureVote con Redis
- ansible-playbook -i hosts.txt 03_playbook.yml --extra-vars "@vars.yml"  
+ansible-playbook -i hosts.txt 03_playbook.yml --extra-vars "@vars.yml"  
 
